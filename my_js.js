e4fa2157d90c9f908a61f1b427311fa896744377
JorgeHenriqueFrassonDev <31277405+JorgeFrasson@users.noreply.github.com>
2019-05-18 12:32:42 -0300

    Update my_js.js

--- my_js.js
+++ my_js.js
@@ -2,21 +2,17 @@ function AlteraEstadoRele(){
     var estado1 = document.getElementById("estado1").innerHTML;
     var estado2 = document.getElementById("estado2").innerHTML;
     if(estado1 === "0"){
-        document.getElementById("estado1").innerHTML="1";
-        document.getElementById("rele1").innerHTML="<div class='rele_ligado'></div>";
-        document.getElementById("botao1").innerHTML="<a href='/?desligar1' class='botao'>Desligar Rele</a>";
+        document.getElementById("botao1").innerHTML="1 On";
+        document.getElementById("botao1").href="/?ligar1";
     } else {
-        document.getElementById("estado").innerHTML="0";
-        document.getElementById("rele1").innerHTML="<div class='rele_desligado'></div>";
-        document.getElementById("botao1").innerHTML="<a href='/?ligar1' class='botao'>Ligar Rele</a>";
+        document.getElementById("botao1").innerHTML="1 Off";
+        document.getElementById("botao1").href="/?desligar1";
     }
-    if(estado2 == "0"){
-        document.getElementById("estado2").innerHTML="1";
-        document.getElementById("rele2").innerHTML="<div class='rele_ligado'></div>";
-        document.getElementById("botao2").innerHTML="<a href='/?desligar2' class='botao'>Desligar Rele</a>";
+    if(estado2 === "0"){
+        document.getElementById("botao2").innerHTML="2 On";
+        document.getElementById("botao2").href="/?ligar2";
     } else {
-        document.getElementById("estado2").innerHTML="0";
-        document.getElementById("rele2").innerHTML="<div class='rele_desligado'></div>";
-        document.getElementById("botao2").innerHTML="<a href='/?ligar2' class='botao'>Ligar Rele</a>";
+        document.getElementById("botao2").innerHTML="2 Off";
+        document.getElementById("botao2").href="/?desligar2";
     }
 }
