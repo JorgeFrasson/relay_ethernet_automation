dc7ba526cd72fb8cadb76c00c6e330d48ff626d8
JorgeHenriqueFrassonDev <31277405+JorgeFrasson@users.noreply.github.com>
2019-05-26 00:38:46 -0300

    Update my_js.js

--- my_js.js
+++ my_js.js
@@ -2,13 +2,13 @@ function AlteraEstadoRele(){
     var estado1 = document.getElementById("estado1").innerHTML;
     var estado2 = document.getElementById("estado2").innerHTML;
     if(estado1 == "0"){
-        document.getElementById("rele1").href="?desligar1";
-    } else {
         document.getElementById("rele1").href="?ligar1";
+    } else {
+        document.getElementById("rele1").href="?desligar1";
     }
     if(estado2 == "0"){
-        document.getElementById("rele2").href="?desligar2";
-    } else {
         document.getElementById("rele2").href="?ligar2";
+    } else {
+        document.getElementById("rele2").href="?desligar2";
     }
 }
